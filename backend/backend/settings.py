be60bff2c36c4e86204949726f082f68afd65a65
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-05-11 17:14:07 +0300

    serever is running

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,10 +72,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('POSTGRES_DB', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default=None),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
-        'HOST': os.getenv('POSTGRES_HOST', default=None),
-        'PORT': os.getenv('POSTGRES_PORT', default=None),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='1234qwerty'),
+        'HOST': os.getenv('POSTGRES_HOST', default='db'),
+        'PORT': os.getenv('POSTGRES_PORT', default='5432'),
     }
 }
 
