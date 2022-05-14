5ccf20b9c7fd7791823def4537c4ac18ee3516c5
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-05-14 18:25:59 +0300

    try push

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_filters.rest_framework import FilterSet, filters
-from rest_framework.filters import SearchFilter
 from recipes.models import Recipe, Tag
+from rest_framework.filters import SearchFilter
 
 
 class IngredientFilter(SearchFilter):
