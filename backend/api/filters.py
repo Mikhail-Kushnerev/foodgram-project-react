a23df6abd78d3e0841cc460754be4cc70d8210bd
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-05-14 21:52:12 +0300

    finaly

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django_filters.rest_framework import FilterSet, filters
+
 from recipes.models import Ingredient, Recipe, Tag
 
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,13 @@
 from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from recipes.models import (AmountOfIngrediend, CartShopping, Favourite,
-                            Ingredient, Recipe, Tag)
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
+from recipes.models import (AmountOfIngrediend, CartShopping, Favourite,
+                            Ingredient, Recipe, Tag)
 from users.models import Subscription, User
 
 from .filters import IngredientFilter, UserRecipeFilter
