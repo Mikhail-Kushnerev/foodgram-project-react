19a0ba1ffe028b0cf49e7bb038efead601f26f0c
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-05-18 20:43:15 +0300

    try add 2 pages to pdf

--- backend/api/views.py
+++ backend/api/views.py
@@ -203,7 +203,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         # # page.setPageSize(A4)
         # page.save()
         response = HttpResponse(
-            bytes(page.output()),
+            page.output(),
             content_type='application/pdf'
         )
         response['Content-Disposition'] = (
